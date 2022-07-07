50b699beed57ec973bbd217af3237b38664e2d21
Benjamin Nabet <benjamin.nabet@preligens.com>
2022-07-07 16:56:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const name = "Ben";
+
+  function alertName(name) {
+    alert(name);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button data-testid="alert-button" onClick={() => alertName(name)}>
+        Launch
+      </button>
     </div>
   );
 }
